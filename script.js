448732be260a69e532914c9dbc1b217325e239fb
LakshmiRamalingam <104923036+LakshmiRamalingam@users.noreply.github.com>
2023-11-04 18:49:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
+
 
 function inputDigit(digit) {
   const { displayValue, waitingForSecondOperand } = calculator;
